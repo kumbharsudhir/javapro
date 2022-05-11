b2cf1c3634de04d677e27bca2185f7c8e117719b
kumbharsudhir <105354469+kumbharsudhir@users.noreply.github.com>
2022-05-11 16:32:41 +0530

    Update firstpro.java
    add one more statement Complie Error

--- firstpro.java
+++ firstpro.java
@@ -2,6 +2,8 @@ import java.io.*;
 public class firstpro {
 	public static void main(String args[])
 	{	  
-		  System.out.println("Hello Developers");  
+		  System.out.println("Hello Developers"); 
+		System.out.println("Hello My Dear Friends);  
+	}
 	}
 }
